c1b03944d93f343b944c1726465f307b697634dc
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-09-01 18:54:47 +0300

    edit db

--- routes/notification.js
+++ routes/notification.js
@@ -14,35 +14,23 @@ router.get('/', function(req, res, next) {
 });
 
 /* PUT (update) users listing. */
-router.put('/', function (req, res, next) {
-  const body = req.body;
-  let obj = null;
-
-  if (body && body.id) {
-    fs.readFile('db-notification.json', 'utf8', function (err, data) {
-      if (err) {
-        res.send(500);
-      } else {
-        obj = JSON.parse(data);
-        obj = obj.map(item => {
-          if (item.id === body.id) {
-            return body;
-          }
-        });
-
-        fs.writeFile('db-notification.json', JSON.stringify(obj), 'utf8', function (err, data) {
-          if (err) {
-            res.send(500);
-          } else {
-            res.json({
-              success: true,
-              data: obj
-            })
-          }
-        });
-      }
-    });
-  }
+router.put('/:name', function (req, res, next) {
+  fs.readFile('db-notification.json', 'utf8', function (err, data) {
+    if (err) {
+      res.send(500);
+    }
+    if (req.params.name) {
+      let arr = [];
+      (JSON.parse(data)).forEach(item => {
+        if (item.name === req.params.name) {
+          arr.push(item);
+        }
+      });
+      res.json(arr);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
 
 /* DELETE users listing. */
